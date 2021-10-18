7eff5c9c8843a4032a305ea2a0fad34568e9d6ab
hanyounho <hanyounho@gmail.com>
2021-10-18 11:09:35 +0900

    게시글 등록 화면 생성, 등록 api 연결

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -11,4 +11,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/post/save")
+    public String postsSave() {
+        return "post-save";
+    }
 }
