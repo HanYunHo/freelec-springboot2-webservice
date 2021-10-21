7a129b528289749d5f3b5ad5e85599dca7ac74cd
hanyounho <hanyounho@gmail.com>
2021-10-21 15:25:02 +0900

    5.4 로그인 기능 개선 글목록이 정상적으로 출력되지 않는 문제 발생

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
 import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
@@ -16,13 +17,11 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(user != null){
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("post", postsService.findAllDesc());
+        if (user != null) {
             model.addAttribute("userName", user.getName());
         }
         return "index";
